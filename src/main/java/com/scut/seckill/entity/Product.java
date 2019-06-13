b6bee1f57ec22d6bf61abc49126ea11647e6c4ac
xiaoxiu112233 <2337606566@qq.com>
2019-06-13 12:29:33 +0800

    秒杀系统

--- src/main/java/com/scut/seckill/entity/Product.java
+++ src/main/java/com/scut/seckill/entity/Product.java
@@ -39,5 +39,53 @@ public class Product {
 	public Product(Integer id){
 		this.id = id;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
 }
   
